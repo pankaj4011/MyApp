ade5b14e16b3c880ce9c897fbed7727d284b836f
pankaj4011 <pankaj4011@gmail.com>
2020-08-05 10:43:24 +0530

    Adding pom.xml and product.java

---
+++ src/main/java/com/ofss/Product.java
@@ -0,0 +1,23 @@
+package com.ofss;
+
+public class Product {
+String productName;
+double price;
+public Product() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+public String getProductName() {
+	return productName;
+}
+public void setProductName(String productName) {
+	this.productName = productName;
+}
+public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
+
+}
